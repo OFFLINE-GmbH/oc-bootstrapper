c29bfde0e3a4fabd9c49d66961da19c455680055
Tobias Kündig <tobias.kuendig@offlinegmbh.ch>
2016-04-30 18:45:14 +0200

    Fixed global include path

--- src/Util/UsesTemplate.php
+++ src/Util/UsesTemplate.php
@@ -1,15 +1,12 @@
 <?php
-
-
 namespace OFFLINE\Bootstrapper\October\Util;
-
 
 trait UsesTemplate
 {
     public function getTemplate($file)
     {
         $dist      = __DIR__ . DS . implode(DS, ['..', '..', 'templates', $file]);
-        $overwrite = __DIR__ . DS . implode(DS, ['..', '..', '..', '..', 'october', $file]);
+        $overwrite = __DIR__ . DS . implode(DS, ['..', '..', '..', '..', '..', 'october', $file]);
 
         return file_exists($overwrite) ? realpath($overwrite) : realpath($dist);
     }
