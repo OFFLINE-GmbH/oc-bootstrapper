324baca289dafe955f1380449487ca6f14238a2d
Tobias Kündig <tobias@offline.swiss>
2016-08-02 17:29:48 +0200

    Updated README

--- templates/Envoy.blade.php
+++ templates/Envoy.blade.php
@@ -13,8 +13,8 @@
     git pull
     [ ! -f "composer.phar" ] && wget https://getcomposer.org/composer.phar
     php composer.phar install --no-interaction --no-dev --prefer-dist
-## Enable this line when using bare repos
-# ./vendor/bin/october install
+    ## Enable this line when using bare repos
+    # ./vendor/bin/october install
     php artisan -v october:up
     git status -s
 @endtask
