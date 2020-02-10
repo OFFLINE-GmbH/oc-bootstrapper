d8a3b68359c08792d239b00e1d2711822347ad69
Tobias Kündig <tobias@offline.swiss>
2020-02-10 07:53:34 +0100

    Fixed platform setting

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -428,7 +428,7 @@ class InstallCommand extends Command
             return;
         }
 
-        if (isset($structure->config)) {
+        if (isset($structure->config->platform->php)) {
             $structure->config->platform->php = '7.2.0'; // Minimum required version by cypresslab/gitelephant
         }
 
